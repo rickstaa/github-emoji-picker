81555072d6d731cdf3ba694317dbc3ed04aab0c4
Rick Staa <rick.staa@outlook.com>
2022-09-17 18:36:22 +0200

    refactor: create themeSwitch component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+/**
+ * @file Main App file.
+ */
 import Picker from "@emoji-mart/react";
 import {
   Alert,
@@ -8,15 +11,16 @@ import {
   Snackbar,
   Typography,
 } from "@mui/material";
-import { createTheme, styled, ThemeProvider } from "@mui/material/styles";
-import Switch from "@mui/material/Switch";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
 import useMediaQuery from "@mui/material/useMediaQuery";
 import { useEffect, useState } from "react";
 import GitHubButton from "react-github-btn";
+import { ThemeSwitch } from "./components/ThemeSwitch";
 import customGithubEmojis from "./data/github_custom_emojis.json";
 import data from "./data/github_emojis.json";
 
-/* Types */
+/** Types */
 export interface SnackbarMessage {
   message: string;
   key: number;
@@ -26,53 +30,6 @@ export interface State {
   snackPack: readonly SnackbarMessage[];
   messageInfo?: SnackbarMessage;
 }
-
-const MaterialUISwitch = styled(Switch)(({ theme }) => ({
-  width: 62,
-  height: 34,
-  padding: 7,
-  "& .MuiSwitch-switchBase": {
-    margin: 1,
-    padding: 0,
-    transform: "translateX(6px)",
-    "&.Mui-checked": {
-      color: "#fff",
-      transform: "translateX(22px)",
-      "& .MuiSwitch-thumb:before": {
-        backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
-          "#fff"
-        )}" d="M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z"/></svg>')`,
-      },
-      "& + .MuiSwitch-track": {
-        opacity: 1,
-        backgroundColor: theme.palette.mode === "dark" ? "#8796A5" : "#aab4be",
-      },
-    },
-  },
-  "& .MuiSwitch-thumb": {
-    backgroundColor: theme.palette.mode === "dark" ? "#003892" : "#001e3c",
-    width: 32,
-    height: 32,
-    "&:before": {
-      content: "''",
-      position: "absolute",
-      width: "100%",
-      height: "100%",
-      left: 0,
-      top: 0,
-      backgroundRepeat: "no-repeat",
-      backgroundPosition: "center",
-      backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
-        "#fff"
-      )}" d="M9.305 1.667V3.75h1.389V1.667h-1.39zm-4.707 1.95l-.982.982L5.09 6.072l.982-.982-1.473-1.473zm10.802 0L13.927 5.09l.982.982 1.473-1.473-.982-.982zM10 5.139a4.872 4.872 0 00-4.862 4.86A4.872 4.872 0 0010 14.862 4.872 4.872 0 0014.86 10 4.872 4.872 0 0010 5.139zm0 1.389A3.462 3.462 0 0113.471 10a3.462 3.462 0 01-3.473 3.472A3.462 3.462 0 016.527 10 3.462 3.462 0 0110 6.528zM1.665 9.305v1.39h2.083v-1.39H1.666zm14.583 0v1.39h2.084v-1.39h-2.084zM5.09 13.928L3.616 15.4l.982.982 1.473-1.473-.982-.982zm9.82 0l-.982.982 1.473 1.473.982-.982-1.473-1.473zM9.305 16.25v2.083h1.389V16.25h-1.39z"/></svg>')`,
-    },
-  },
-  "& .MuiSwitch-track": {
-    opacity: 1,
-    backgroundColor: theme.palette.mode === "dark" ? "#8796A5" : "#aab4be",
-    borderRadius: 20 / 2,
-  },
-}));
 
 const darkTheme = createTheme({
   palette: {
@@ -99,6 +56,12 @@ const dayTheme = createTheme({
     },
   },
 });
+
+const customEmojiCategories = {
+  github: {
+    svg: '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid meet" viewBox="0 0 25px 25px"><path d="M12 1.27a11 11 0 00-3.48 21.46c.55.09.73-.28.73-.55v-1.84c-3.03.64-3.67-1.46-3.67-1.46-.55-1.29-1.28-1.65-1.28-1.65-.92-.65.1-.65.1-.65 1.1 0 1.73 1.1 1.73 1.1.92 1.65 2.57 1.2 3.21.92a2 2 0 01.64-1.47c-2.47-.27-5.04-1.19-5.04-5.5 0-1.1.46-2.1 1.2-2.84a3.76 3.76 0 010-2.93s.91-.28 3.11 1.1c1.8-.49 3.7-.49 5.5 0 2.1-1.38 3.02-1.1 3.02-1.1a3.76 3.76 0 010 2.93c.83.74 1.2 1.74 1.2 2.94 0 4.21-2.57 5.13-5.04 5.4.45.37.82.92.82 2.02v3.03c0 .27.1.64.73.55A11 11 0 0012 1.27" /></svg>',
+  },
+};
 
 const App = () => {
   const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
@@ -159,24 +122,6 @@ const App = () => {
   console.log(data);
   console.log(customGithubEmojis);
   const customEmojis = [customGithubEmojis];
-
-  const custom = [
-    {
-      name: "GitHub",
-      emojis: [
-        {
-          id: "octocat",
-          name: "Octocat",
-          keywords: ["octocat"],
-          skins: [
-            {
-              src: "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
-            },
-          ],
-        },
-      ],
-    },
-  ];
 
   return (
     <ThemeProvider theme={mode === "dark" ? darkTheme : dayTheme}>
@@ -274,7 +219,7 @@ const App = () => {
             </Grid>
           </Grid>
           <Grid item>
-            <MaterialUISwitch
+            <ThemeSwitch
               checked={mode !== "dark"}
               onClick={handleThemeChange}
             />
@@ -289,11 +234,7 @@ const App = () => {
             <Picker
               data={data}
               custom={customEmojis}
-              categoryIcons={{
-                github: {
-                  src: "https://cdns.iconmonstr.com/wp-content/releases/preview/2012/240/iconmonstr-github-1.png",
-                },
-              }}
+              categoryIcons={customEmojiCategories}
               onEmojiSelect={copyClipboard}
               theme={mode === "dark" ? "dark" : "light"}
               sx={{ pt: "1", pb: "1" }}
---
+++ src/components/ThemeSwitch/index.ts
@@ -0,0 +1,1 @@
+export { ThemeSwitch } from "./ThemeSwitch";
