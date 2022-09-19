7bb2109bda24d4bcea99ed922cfb498d03421d9b
rickstaa <rick.staa@outlook.com>
2022-09-19 17:39:01 +0200

    tests: fix tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,8 +4,27 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
+// == Mocks ==
+// MOCK react-github-btn
+// TODO: Can be removed if https://github.com/buttons/react-github-btn/issues/27 is fixed.
+jest.mock("react-github-btn", () => () => <>Button</>);
+
+// Mock emoji-mart
+jest.mock("./components/EmojiPicker", () => ({
+  EmojiPicker: (props: any) => (
+    <div>
+      {props.title} {props.description}
+    </div>
+  ),
+}));
+
+/**
+ * App rendering test.
+ */
 test("renders App", () => {
   render(<App />);
-  const textElement = screen.getByText(/A small tool/i);
+  const textElement = screen.getByText(
+    /A simple emotion picker that displays all /i
+  );
   expect(textElement).toBeInTheDocument();
 });
