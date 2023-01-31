77fd3bc247a99502f1c76b3a5a0d538abcb3704c
rickstaa <rick.staa@outlook.com>
2023-01-31 12:49:42 +0100

    test: fix tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,7 +6,7 @@ import App from "./App";
 
 // == Mocks ==
 
-// Mock emoji-mart
+// Mock emoji-mart.
 jest.mock("./components/EmojiPicker", () => ({
   EmojiPicker: (props: any) => (
     <div>
@@ -15,13 +15,28 @@ jest.mock("./components/EmojiPicker", () => ({
   ),
 }));
 
+// Mock react-i18next.
+jest.mock('react-i18next', () => ({
+  // this mock makes sure any components using the translate hook can use it without a warning being shown
+  useTranslation: () => {
+    return {
+      t: (str: string) => str,
+      i18n: {
+        changeLanguage: () => new Promise(() => {}),
+      },
+    };
+  },
+  initReactI18next: {
+    type: '3rdParty',
+    init: () => {},
+  }
+}));
+
 /**
  * App rendering test.
  */
 test("renders App", () => {
   render(<App />);
-  const textElement = screen.getByText(
-    /A simple emotion picker that displays all /i
-  );
+  const textElement = screen.getByText("GitHub Emoji Picker");
   expect(textElement).toBeInTheDocument();
 });
